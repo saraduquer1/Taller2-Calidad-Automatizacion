3a56e40525f74dbf21c88e8865646daf4368a1c7
Sara Lorena Duque Ramirez <sara.duque1@udea.edu.co>
2025-07-11 12:14:29 -0500

    Actualizacion

--- src/main/java/co/com/udea/orangehrm/myinfo/utils/Constants.java
+++ src/main/java/co/com/udea/orangehrm/myinfo/utils/Constants.java
@@ -11,17 +11,6 @@ public class Constants {
     public static final String USERNAME = "Admin";
     public static final String PASSWORD = "admin123";
 
-    // Datos de prueba
-    public static final String TEST_FIRST_NAME = "Juan";
-    public static final String TEST_LAST_NAME = "Perez";
-    public static final String TEST_EMAIL = "juan.perez@udea.edu.co";
-
-    // Mensajes esperados
-    public static final String SUCCESS_MESSAGE = "Successfully Updated";
-    public static final String DASHBOARD_TITLE = "Dashboard";
-    public static final String REQUIRED_MESSAGE = "Required";
-    public static final String SUCCESS_TITLE = "Success";
-
     // Configuración de tiempos de espera para Toast
     public static final int TOAST_MAX_ATTEMPTS = 50;
     public static final Duration TOAST_WAIT_BETWEEN_ATTEMPTS = Duration.ofMillis(100);
